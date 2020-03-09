14856018457a6e08d366e2f266e1673fc959ac33
kondo <14jk109@ms.dendai.ac.jp>
2020-03-10 00:52:07 +0900

    処理を追加したい

--- src/main/java/ml/mykwlab/creator/Creator.java
+++ src/main/java/ml/mykwlab/creator/Creator.java
@@ -1,16 +1,43 @@
 package ml.mykwlab.creator;
 
 import ml.mykwlab.compile.CompileClasses;
+import ml.mykwlab.compile.CompileException;
+import ml.mykwlab.unittest.Result;
 import org.apache.commons.text.StringSubstitutor;
 import org.apache.lucene.search.spell.LevensteinDistance;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static ml.mykwlab.compile.Compile.compile;
+
 
 public class Creator {
+
+    public static void main(String[] args) throws IOException, CompileException {
+        String template = readFile("RucksackTest.java_template");
+        String gum = readFile("Gum.java");
+        String rucksack = readFile("Rucksack2.java");
+
+        CompileClasses compileClasses = compile(rucksack, Collections.singletonList(gum));
+
+        String[] methodArray = template.split("@Test");
+        System.out.println(templateToJava(methodArray[1], compileClasses));
+
+    }
+
+    // debug
+    private static String readFile(String fileName) throws IOException {
+        Path file = Paths.get("src/main/resources/test/" + fileName);
+        return Files.lines(file).collect(Collectors.joining(System.lineSeparator()));
+    }
+
     public static String templateToJava(String template, CompileClasses compileClasses) {
         Set<ClassStructure> classStructureSet = new HashSet<>();
         classStructureSet.add(new ClassStructure(compileClasses.getTargetClass()));
---
+++ src/main/java/ml/mykwlab/creator/DynamicTest.java
@@ -0,0 +1,8 @@
+package ml.mykwlab.creator;
+
+/**
+ * 実行してない数もカウントする
+ * インスタンスが必要
+ */
+public class DynamicTest {
+}
